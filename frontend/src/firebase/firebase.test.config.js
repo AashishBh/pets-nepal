8d010830e21ab31f8f6fa21f7c6f003e68509aa5
lilixac <hinewton25@gmail.com>
2021-02-04 16:04:22 +0545

    Format for firebase.config.js

---
+++ frontend/src/firebase/firebase.test.config.js
@@ -0,0 +1,15 @@
+// Also, rename the file as firebase.config.js. It's listed in the .gitignore file.
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "<get key from the firebase console>",
+  authDomain: "<get key from the firebase console>",
+  databaseURL: "<get key from the firebase console>",
+  projectId: "<get key from the firebase console>",
+  storageBucket: "<get key from the firebase console>",
+  messagingSenderId: "<get key from the firebase console>",
+  appId: "<get key from the firebase console>",
+  measurementId: "<get key from the firebase console>"
+};
+
+export default firebaseConfig;
