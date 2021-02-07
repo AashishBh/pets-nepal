c9b0d3c8e34759acca89f68fdc051c0c104a5df2
lilixac <hinewton25@gmail.com>
2021-02-07 11:20:03 +0545

    No idea what I changed

--- frontend/src/adoption/components/pet.jsx
+++ frontend/src/adoption/components/pet.jsx
@@ -4,27 +4,31 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 const Pet = (props) => {
-	const text = String(props.description)
+	const text = String(props.description);
 	return (
-			<Card style={{ width: '18rem' }}>
-				<Card.Img variant="top" height="250" width="250" src={props.imageLink}/>
+		<Card style={{ width: "18rem" }}>
+			<Card.Img
+				variant="top"
+				height="250"
+				width="250"
+				src={props.imageLink}
+			/>
 
-				<Button> Adopt </Button>
-				<Card.Body>
-					<Card.Title>{props.petName}</Card.Title>
-					<Card.Text>
-					{renderHTML(text)}
-					</Card.Text>
-				</Card.Body>
-				<Card.Footer>
-					<small className="text-muted">
-						by: {props.by}
-						<br />
-						on: {Date(props.date).toLocaleString("en-US")}
-					</small>
-				</Card.Footer>
-			</Card>
-
+			<Button> Adopt </Button>
+			<Card.Body>
+				<Card.Title>{props.petName}</Card.Title>
+				<Card.Text>
+					<small>{renderHTML(text)}</small>
+				</Card.Text>
+			</Card.Body>
+			<Card.Footer>
+				<small className="text-muted">
+					by: {props.by}
+					<br />
+					on: {Date(props.date).toLocaleString("en-US")}
+				</small>
+			</Card.Footer>
+		</Card>
 	);
 };
 
