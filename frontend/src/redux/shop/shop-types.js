11d151e7eb4176341695bd11c965380384860b47
lilixac <hinewton25@gmail.com>
2021-02-08 21:55:56 +0545

    deleted redundant redux, use of redux browser extension

--- frontend/src/redux/shop/shop-types.js
+++
@@ -1,8 +0,0 @@
-const ShopActionTypes = {
-	CAT_DATA: "CAT_DATA",
-	DOG_DATA: "DOG_DATA",
-	FISH_DATA: "FISH_DATA",
-	OTHERS_DATA: "OTHERS_DATA"
-};
-
-export default ShopActionTypes;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+import { createStore } from "redux";
+// import { createStore, applyMiddleware } from "redux";
+// import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
-const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// const middlewares = [logger];
+const store = createStore(
+	rootReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
