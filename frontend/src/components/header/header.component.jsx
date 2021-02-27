74f5e066f2d924bac7d2f5a346c3f97464dbcab0
lilixac <hinewton25@gmail.com>
2021-02-27 13:00:59 +0545

    Implemented lost and found, placed the in the navbar

--- frontend/src/components/header/header.component.jsx
+++ frontend/src/components/header/header.component.jsx
@@ -46,7 +46,14 @@ const Header = ({ currentUser, productCount }) => {
 							</Link>
 						</Navbar>
 					</Nav>
-					<Nav >
+					<Nav>
+						<Navbar>
+							<Link className={style.links} to="/lostnfound">
+								Lost & Found
+							</Link>
+						</Navbar>
+					</Nav>
+					<Nav>
 						<NavDropdown
 							title="Services"
 							className={style.dropdownmenu}
