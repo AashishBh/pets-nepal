4463450ceb11a7c70f4bb588b1245a729736930d
AashishBh <aaceesh.bhandary@gmail.com>
2021-12-14 16:21:02 +0545

    git ignore modified

---
+++ frontend/src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDUcVctgzx7otDLj_BlqY8mn_Lj-U7wyDM",
+    authDomain: "minor-2b2f5.firebaseapp.com",
+    databaseURL: "https://minor-2b2f5.firebaseio.com",
+    projectId: "minor-2b2f5",
+    storageBucket: "minor-2b2f5.appspot.com",
+    messagingSenderId: "297884480341",
+    appId: "1:297884480341:web:9286277434ebca667bf5dc",
+    measurementId: "G-F8E6NTXSTM"
+  };
+  
+  export default firebaseConfig;
